69db58a139dd9091780619b3cc5b8d111d33f73a
Anton Safonov <anton@tonek.org>
2025-06-13 20:06:07 -0700

    Split library from web.

--- scripts/gen_protos.sh
+++ scripts/gen_protos.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 root=$(git rev-parse --show-toplevel)
 mkdir -p "${root}"/server/src/dev_observer/api/
-mkdir -p "${root}"/web/packages/dev-observer-api/src/pb/
+mkdir -p "${root}"/web/packages/api/src/pb/
 
 PROTO_FILES=$(find proto -name "*.proto" | sed 's|^proto/||')
 
@@ -22,6 +22,6 @@ find "${root}"/server/src/dev_observer/api/ -type d -exec touch {}/__init__.py \
 protoc \
   -I "${root}/proto"  \
   --plugin=./web/node_modules/.bin/protoc-gen-ts_proto \
-  --ts_proto_out=web/packages/dev-observer-api/src/pb \
+  --ts_proto_out=web/packages/api/src/pb \
   --ts_proto_opt=globalThisPolyfill=true,unrecognizedEnum=false,oneof=unions-value,outputServices=nice-grpc,outputServices=generic-definitions,useExactTypes=false \
   ${PROTO_FILES}
