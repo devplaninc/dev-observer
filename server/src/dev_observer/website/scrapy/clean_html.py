fd7f19d1f5286162ae418d1748c368deb72b21e3
Anton Safonov <anton@tonek.org>
2025-07-16 16:35:29 -0700

    More verbose crawling.

--- server/src/dev_observer/website/scrapy/clean_html.py
+++ server/src/dev_observer/website/scrapy/clean_html.py
@@ -10,18 +10,18 @@ def clean_html_for_llm(html: str) -> str:
     for comment in soup.find_all(string=lambda s: isinstance(s, Comment)):
         comment.extract()
 
-    junk_keywords = ["sidebar", "popup", "ad", "sponsor", "subscribe", "cookie"]
-    for el in soup.find_all(True):  # True = all tags
-        if el.attrs and any(attr in el.attrs for attr in ["class", "id"]):
-            class_id_values = " ".join(el.get("class", []) + [el.get("id", "")])
-            if any(word in class_id_values.lower() for word in junk_keywords):
-                el.decompose()
-
-    attrs_to_clear = ["class", "style"]
-    for tag in soup.find_all(True):
-        if tag.attrs:
-            for attr in attrs_to_clear:
-                tag.attrs.pop(attr, None)
+    # junk_keywords = ["sidebar", "popup", "ad", "sponsor", "subscribe", "cookie"]
+    # for el in soup.find_all(True):  # True = all tags
+    #     if el.attrs and any(attr in el.attrs for attr in ["class", "id"]):
+    #         class_id_values = " ".join(el.get("class", []) + [el.get("id", "")])
+    #         if any(word in class_id_values.lower() for word in junk_keywords):
+    #             el.decompose()
+    #
+    # attrs_to_clear = ["class", "style"]
+    # for tag in soup.find_all(True):
+    #     if tag.attrs:
+    #         for attr in attrs_to_clear:
+    #             tag.attrs.pop(attr, None)
 
     clean = soup
 
